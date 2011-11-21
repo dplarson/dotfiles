43665da77e79ae38cba500debae94014b013c6cf
David Larson <dlarson2@ucmerced.edu>
2011-11-21 00:51:23 -0800

    Shortcut to copy both DSA and RSA public keys

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,4 @@
 # Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"#
+alias pubdsa="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public DSA key copied to pasteboard.'"#
+alias pubrsa="more ~/.ssh/id_rsa.public | pbcopy | echo '=> Public RSA key copied to pasteboard.'"#
+
