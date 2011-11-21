a0472bef8b9315b3de3eea840a5a5d6396100b3f
David Larson <dlarson2@ucmerced.edu>
2011-11-21 01:09:19 -0800

    Minor changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -36,8 +36,6 @@ alias o='open .'
 
 # misc
 alias reload='source ~/.zshrc'                                  # reload zsh settings
-alias off='/usr/sbin/networksetup -setairportpower en1 off'     # wifi off
-alias on='/usr/sbin/networksetup -setairportpower en1 on'       # wifi on
 alias showcal='cal | sed "s/.*/ & /;s/ $(date +%e) / [] /"'     # show calendar
 
 # Brew
@@ -47,22 +45,3 @@ alias brews='brew list -l'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
-
-# Recursively delete `.DS_Store` files
-alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
-
-# Kill processes
-alias killmenu='killall SystemUIServer' 		# top menubar
-alias killdock='killall Dock'                   # Dock
-alias killfinder='killall Finder'               # Finder
-
-# Empty the Trash
-alias emptytrash="rm -rfv ~/.Trash"
-
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
-
-# Hide/show all desktop icons (useful when presenting)
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
