a3c68868fbd17f4612ae6e3ce20bea3f8ee6507c
David Larson <dlarson2@ucmerced.edu>
2011-11-24 21:19:39 -0800

    Added prompt character that changes depending on whether inside a git branch

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
-# Custom colors
+# Custom colors (r,g,b)
 # -black:    0,0,0
 # -red:      255,27,0
 # -green:    72,235,0
@@ -8,11 +8,18 @@
 # -cyan:     0,146,209
 # -white:    242,242,242
 
-#   -color for Terminal theme "IR_Black"
-export PROMPT=$'\n\e[1;33m%n\e[0m at \e[1;35m%m\e[0m in \e[0;34m${PWD/#$HOME/~}\e[0m \n> '
-#export RPROMPT=''
+# function to change prompt character depending whether inside a git branch
+# 	-adaptaed from Steve Losh's zsh prompt (stevelosh.com)
+function prompt_char {
+	git branch >/dev/null 2>/dev/null && echo '±' && return
+	echo '>'
+}
 
-# Title
+# prompt
+export PROMPT=$'\n\e[1;33m%n\e[0m at \e[1;35m%m\e[0m in \e[0;34m${PWD/#$HOME/~}\e[0m \n$(prompt_char) '
+export RPROMPT=''
+
+# terminal window title
 precmd() {
     title "zsh" "%m - zsh" "%55<...<%~"
 }
