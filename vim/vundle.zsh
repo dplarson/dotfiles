507c31298f2f3c486df83a829821dd7527161364
David Larson <dlarson2@ucmerced.edu>
2012-03-16 16:23:50 -0700

    Add vundle autoload functions

---
+++ vim/vundle.zsh
@@ -0,0 +1,26 @@
+# Taken from oh-my-zsh
+# Github: https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/vundle/vundle.plugin.zsh
+
+function vundle-init () {
+  if [ ! -d ~/.vim/bundle/vundle/ ]
+  then
+    mkdir -p ~/.vim/bundle/vundle/
+  fi
+
+  if [ ! -d ~/.vim/bundle/vundle/.git/ ]
+  then
+    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    echo "\n\tRead about vim configuration for vundle at https://github.com/gmarik/vundle\n"
+  fi
+}
+
+function vundle () {
+  vundle-init
+  vim -c "execute \"BundleInstall\" | q | q"
+}
+
+
+function vundle-update () {
+  vundle-init
+  vim -c "execute \"BundleInstall!\" | q | q"
+}
