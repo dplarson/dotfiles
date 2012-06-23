cebefe360c72c8fa8778598e3c3f3c0cb23d81fe
David Larson <dlarson2@ucmerced.edu>
2012-06-22 23:22:13 -0700

    Added kill autocompletition

--- zsh/completition.zsh
+++ zsh/completition.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# kill process completition
+zstyle ':completion:*:kill:*' menu yes select
+zstyle ':completion:*:kill:*' force-list always
+zstyle ':completion:*:processes' command 'ps x -o pid,pcpu,tt,args'
