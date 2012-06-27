bfb185565349c85718613655f12252817b6fa215
David Larson <dplarson@ucsd.edu>
2012-06-26 23:12:37 -0700

    Show commit author name in blue

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,4 +10,4 @@ alias gb='git branch'
 # Analysis
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty=format:'%Cred%h %Cblue%an%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
