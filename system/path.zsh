47af2334bb1f151843af1842fbc767218724ee83
David Larson <dplarson@ucsd.edu>
2012-11-19 23:16:21 -0800

    Split $PATH for readability.

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,11 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/share/python:/usr/texbin:/usr/local/Cellar/gems/1.8/bin:/usr/sbin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+export PATH="$ZSH/bin:$PATH"
+export PATH="/usr/local/share/python:$PATH"
+export PATH="/usr/texbin:$PATH"
+export PATH="/usr/local/Cellar/gems/1.8/bin:$PATH"
+export PATH="/usr/sbin:$PATH"
+export PATH="~/anaconda/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 
