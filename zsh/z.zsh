ce717b65fcc2c162d93d24010a71202292c84f6f
David Larson <dplarson@ucsd.edu>
2012-12-12 16:02:13 -0800

    Make z.sh setup only for OSX.

--- zsh/z.zsh
+++ zsh/z.zsh
@@ -1,14 +1,18 @@
-# assumes z.sh installed using homebrew
-source $(brew --prefix)/etc/profile.d/z.sh
+# Setup z.sh for OSX
+if [ $(uname -s) = "Darwin" ] ; then
+    
+    # assumes z.sh installed using homebrew
+    source $(brew --prefix)/etc/profile.d/z.sh
 
-# creates empty z datafile if it doesn't already exist
-if [ ! -f $HOME/.z ] ; then
-    echo "z datafile (~/.z) does not exist."
-    echo "Creating it now..."
-    touch $HOME/.z
+    # creates empty z datafile if it doesn't already exist
+    if [ ! -f $HOME/.z ] ; then
+        echo "z datafile (~/.z) does not exist."
+        echo "Creating it now..."
+        touch $HOME/.z
+    fi
+
+    # required to build-up z datafile
+    function precmd () {
+        z --add "$(pwd -P)"
+    }
 fi
-
-# required to build-up z datafile
-function precmd () {
-  z --add "$(pwd -P)"
-}
