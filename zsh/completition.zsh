8e9841e5b2c42f4f6c1d96fb133512030568b39b
David Larson <dplarson@ucsd.edu>
2012-12-15 14:06:20 -0800

    Ignore non-txt files for vim completion in zsh.

--- zsh/completition.zsh
+++ zsh/completition.zsh
@@ -8,3 +8,6 @@ zstyle ':completion:*' insert-tab pending
 zstyle ':completion:*:kill:*' menu yes select
 zstyle ':completion:*:kill:*' force-list always
 zstyle ':completion:*:processes' command 'ps x -o pid,pcpu,tt,args'
+
+# ignore non-text files for e, vi(m), vim
+zstyle ':completion:*:(e|vi|vim|mvim):*' ignored-patterns '*.(o|a|so|aux|dvi|log|swp|fig|bbl|blg|bst|idx|ind|out|toc|class|pdf|ps|eps|pyc|jpg|jpeg|tiff|png|gif)'
