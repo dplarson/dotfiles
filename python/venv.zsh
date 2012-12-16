8f85525d810c3d62a1a8078410321aa8657efb38
David Larson <dplarson@ucsd.edu>
2012-12-16 00:18:51 -0800

    Add autoenv for managing Python virtualenvs
    Autoenv automatically activates a virtualenv is a .env is found. Which
    means you can cd into a project with a .env and start working without
    bothering to remember to switch to the needed virtualenv. Also you can
    use **use_env** from the commandline to create a virtualenv.

---
+++ python/venv.zsh
@@ -0,0 +1,24 @@
+# autoenv
+
+# source virtualenvwrapper and autoenv
+if [ $(uname -s) = "Darwin" ] ; then
+    . $(which virtualenvwrapper.sh)
+    . /usr/local/opt/autoenv/activate.sh
+fi
+
+# command to create new env for autoenv
+use_env () {
+    typeset venv
+    venv="$1"
+    if [[ `basename "${VIRTUAL_ENV:t}"` != "$venv" ]]; then
+        if workon | grep $venv > /dev/null; then
+            workon "$venv"
+        else
+            echo -n "Create virtualenv $venv now? (Yn) "
+            read answer
+            if [[ "$answer" == "Y" ]]; then
+                mkvirtualenv "$venv"
+            fi
+        fi
+    fi
+}
