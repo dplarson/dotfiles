a841d29bc1dc41681ecc9ef3b17109f261148b5d
David Larson <dplarson@ucsd.edu>
2012-12-30 23:29:25 -0800

    Set PATH and MANPATH based on OS.

--- system/path.zsh
+++ system/path.zsh
@@ -1,18 +1,23 @@
-# set system path
-PATH="/usr/local/bin:$PATH"
-PATH="$PATH:/usr/local/sbin"
-PATH="$PATH:$ZSH/bin"
-PATH="$PATH:/usr/local/share/python"
-PATH="$PATH:/usr/texbin"
-PATH="$PATH:/usr/local/Cellar/gems/1.8/bin"
-PATH="$PATH:/usr/sbin"
-export PATH
+# OSX-specific settings
+if [ $(uname -s) = "Darwin" ] ; then
 
-# set manual path
-MANPATH="/usr/local/git/man:$MANPATH"
-MANPATH="/usr/local/man:$MANPATH"
-export MANPATH
+    # set system path
+    PATH="/usr/local/bin:$PATH"
+    PATH="$PATH:/usr/local/sbin"
+    PATH="$PATH:$ZSH/bin"
+    PATH="$PATH:/usr/local/share/python"
+    PATH="$PATH:/usr/texbin"
+    PATH="$PATH:/usr/local/Cellar/gems/1.8/bin"
+    PATH="$PATH:/usr/sbin"
+    export PATH
 
-# select only unique entries
-typeset -U PATH
-typeset -U MANPATH
+    # set manual path
+    MANPATH="/usr/local/git/man:$MANPATH"
+    MANPATH="/usr/local/man:$MANPATH"
+    export MANPATH
+
+    # select only unique entries
+    typeset -U PATH
+    typeset -U MANPATH
+
+fi
