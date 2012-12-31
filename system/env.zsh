7c889c04a163f665a3dce502225ed32885643442
David Larson <dplarson@ucsd.edu>
2012-12-31 01:39:47 -0800

    Set EDITOR based on system options.
    The revision allows checking of which editors on installed on a aystem
    and sets the EDITOR based on the results (e.g. if mvim exists, then set
    editor as mvim).

--- system/env.zsh
+++ system/env.zsh
@@ -1,7 +1,12 @@
 # set default editor
 
-if [ $(uname -s) = "Darwin" ] ; then
+# editor preference:
+#   1. MacVim/mvim
+#   2. vim
+
+# set editor based on what's found on the system
+if type mvim >/dev/null 2>/dev/null ; then
   export EDITOR='mvim'
-else
+elif type vim >/dev/null 2>/dev/null ; then
   export EDITOR='vim'
 fi
