8eb8bb3f6d39efbbcee0cfc06d6afe0bbaf4497f
David Larson <dplarson@ucsd.edu>
2013-01-09 00:03:01 -0800

    [brew] modify alias brews for easier reading.

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,7 +1,7 @@
 # homebrew command aliases
 
 # list all brews installed
-alias brews='brew list -l'
+alias brews='brew list'
 
 # update brew formulas and list outdated after update
 alias bru='brew update && echo "==> \033[1mOutdated Formulae \033[0m" && brew outdated'
