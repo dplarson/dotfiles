ebbeb781148fe3ca4b527028824ef891c7d3660d
David Larson <dplarson@ucsd.edu>
2013-01-12 12:35:15 -0800

    [osx] remove unused hide and show aliases.

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -13,10 +13,6 @@ if [ $(uname -s) = "Darwin" ] ; then
   # empty the Trash
   alias emptytrash="rm -rfv ~/.Trash"
 
-  # show/hide hidden files in Finder
-  alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
-  alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
-
   # hide/show all desktop icons (useful when presenting)
   alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
   alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
