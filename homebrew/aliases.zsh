13e7b0bcd6df016f9e497795257fe01130a987ba
David Larson <dplarson@ucsd.edu>
2013-01-25 21:12:28 -0800

    [homebrew] improve `bru` alias output.

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,10 +1,25 @@
 # homebrew command aliases
 
+
+brew_no_outdated_pkgs () {
+  # returns nothing if no pkgs are outdated
+  brew outdated >/dev/null 2>&1
+}
+
+brew_check_outdated () {
+  if brew_no_outdated_pkgs ; then
+    echo "==> \033[1mOutdated Formulae \033[0m" && echo "Nothing to upgrade."
+  else
+    echo "==> \033[1mOutdated Formulae \033[0m" && brew outdated
+  fi
+}
+
+
 # list all brews installed
 alias brews='brew list'
 
 # update brew formulas and list outdated after update
-alias bru='brew update && echo "==> \033[1mOutdated Formulae \033[0m" && brew outdated'
+alias bru='brew update && brew_check_outdated'
 
 # list outdated brews
-alias bro='echo "==> \033[1mOutdated Formulae \033[0m" && brew outdated'
+alias bro='brew_check_outdated'
