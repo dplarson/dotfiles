05a6cedfead2d913fb90c63accf4925f77b3c27a
David Larson <dplarson@ucsd.edu>
2013-01-29 00:09:32 -0800

    [zsh] revert to prompt with git branch and sha1
    Left prompt shows user@computer-name, while right prompt shows current
    working directory followed by git-branch@git-sha1 (if in git repo).

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -16,48 +16,29 @@ else
 fi
 
 
-# get current git branch
-git_current_branch () {
-  $git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||'
-}
+git_prompt() {
+   
+  # check if git repo
+  git_status=$($git status 2>/dev/null | tail -n 1)
 
-git_dirty () {
-  st=$($git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]] ; then
-    echo ""
-  else
-    if [[ "$st" =~ ^nothing ]] ; then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
+  if [[ ! $git_status == "" ]] ; then
+    # current branch name
+    git_branch=$($git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||')
+   
+    # short sha1 
+    git_sha1=$($git rev-parse --short HEAD 2>/dev/null)
 
-git_prompt_info () {
-  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-  # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
-  echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]] ; then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    # git prompt (example: master@46a45f4)
+    echo "%{$fg[blue]%}$git_branch%{$fg_bold[blue]%}@%{$fg_bold[red]%}$git_sha1"
   fi
 }
 
 
 # set left prompt
 #
-#   should look similar to this:
-#     user@computer-name
-#     >
+# should look similar to this:
+#   user@computer-name
+#   > git status
 #
 export PROMPT='
 %{$fg_bold[yellow]%}%n%{$fg_bold[blue]%}@%{$fg_bold[yellow]%}%M%{$reset_color%}
@@ -72,8 +53,8 @@ export PROMPT='
 #   otherwise only show current directory:
 #     ~/Documents
 #
-#export RPROMPT=$'%{\e[1;33m%}%~ $(git_prompt_info)%{\e[0m%}'
-export RPROMPT='%{$fg_bold[yellow]%}%~ $(git_dirty)$(need_push)%{$reset_color%}'
+export RPROMPT='%{$fg_bold[yellow]%}%~ %{$reset_color%}$(git_prompt)%{$reset_color%}'
+
 
 # set terminal window title
 precmd() {
