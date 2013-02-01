bc5e7d65a78a35bc7e24fa2323457cac75d20c80
David Larson <dplarson@ucsd.edu>
2013-01-31 21:43:28 -0800

    [homebrew] correct (maybe) alias error.

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -2,7 +2,7 @@
 
 
 brew_check_outdated () {
-  if [ ! "$(brew outdated >/dev/null 2>&1)" = "" ] ; then
+  if [ ! "$(brew outdated)" = "" ] ; then
     echo "==> \033[1mOutdated Formulae \033[0m" && brew outdated
   fi
 }
