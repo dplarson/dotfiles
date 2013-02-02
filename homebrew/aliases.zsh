cf069a62abeb878660060bed137ce948165050b4
David Larson <dplarson@ucsd.edu>
2013-02-01 23:43:28 -0800

    [Homebrew] disable beer emoji

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,12 +1,14 @@
 # homebrew command aliases
 
+# disable beer emoji
+export HOMEBREW_NO_EMOJI=1
 
+# check if any pkgs are outdated
 brew_check_outdated () {
   if [ ! "$(brew outdated)" = "" ] ; then
     echo "==> \033[1mOutdated Formulae \033[0m" && brew outdated
   fi
 }
-
 
 # list all brews installed
 alias brews='brew list'
