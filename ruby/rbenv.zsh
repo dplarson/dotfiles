55855d613710ffbf1ace24631ee6007d4509e75b
David Larson <dplarson@ucsd.edu>
2013-12-03 18:07:13 -0800

    Ruby/rbenv: remove unnecessary code
    Apparently the eval line is all that is actually needed.

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,26 +1,5 @@
 # rbenv
 
-# only run code below if rbenv installed via homebrew
 if [ -x /usr/local/bin/rbenv ] ; then
-
-  # rehash shims
-  rbenv rehash 2>/dev/null
-
-  # shell thing
-  rbenv() {
-    command="$1"
-    if [ "$#" -gt 0 ]; then
-      shift
-    fi
-
-    case "$command" in
-      shell)
-        eval `rbenv "sh-$command" "$@"`;;
-      *)
-        command rbenv "$command" "$@";;
-    esac
-  }
-
-  # enable rbenv
   eval "$(rbenv init -)"
 fi
