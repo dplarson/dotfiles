00f8cbf6a7e1f84761dc2775ad0c9ff8fa80264c
David Larson <dplarson@ucsd.edu>
2014-02-01 15:50:41 -0800

    ZSH: disable beeps

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -17,6 +17,7 @@ autoload -U $DOTFILES/zsh/functions/*(:t)
 setopt NO_BG_NICE    # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
+setopt NO_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS   # allow functions to have local traps
 setopt PROMPT_SUBST
