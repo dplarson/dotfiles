60442fceefb781253e2a63e27166b9647404e550
David Larson <dplarson@ucsd.edu>
2014-07-27 00:09:46 -0700

    hydra: revised config for 1.0 release API
    Hydra is now at the 1.0 release (stable), which is good. But there were
    a few API changes that broke my previous config setup (that this commit
    fixes).

--- hydra/hydra.symlink/init.lua
+++ hydra/hydra.symlink/init.lua
@@ -1,31 +1,19 @@
--- save the time when update are checked
-function checkforupdates()
-  updates.check(function(available)
-      -- what to do when an update is checked
-      if available then
-        notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
-      else
-        hydra.alert("No update available.")
-      end
-  end)
-  settings.set('lastcheckedupdates', os.time())
-end
+-- reload config if changes detected
+pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
 
--- show a helpful menu
-menu.show(function()
-    local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
-    local updatefns = {[true] = updates.install, [false] = checkforupdates}
-    local hasupdate = (updates.newversion ~= nil)
+-- load on login
+hydra.autolaunch.set(true)
 
+hydra.menu.show(function()
     return {
         {title = "Reload Config", fn = hydra.reload},
         {title = "Open REPL", fn = repl.open},
         {title = "-"},
-        {title = "About", fn = hydra.showabout},
-        {title = updatetitles[hasupdate], fn = updatefns[hasupdate]},
-        {title = "Quit Hydra", fn = os.exit},
+        {title = "About Hydra", fn = hydra.showabout},
+        {title = "Quit", fn = os.exit},
     }
 end)
+
 
 -- move the window to the right half of the screen
 function movewindow_righthalf()
@@ -67,25 +55,3 @@ hotkey.new({"alt"}, "1", movewindow_lefthalf):enable()
 hotkey.new({"alt"}, "2", movewindow_righthalf):enable()
 hotkey.new({"alt"}, "3", movewindow_center):enable()
 hotkey.new({"alt"}, "4", movewindow_wide):enable()
-
--- what to do when an update is checked
-function updates.available(available)
-    if available then
-        notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
-    else
-        hydra.alert("No update available.")
-    end
-end
-
--- launch Hydra at login
-autolaunch.set(true)
-
--- check for updates every week
-timer.new(timer.weeks(1), checkforupdates):start()
-notify.register("showupdate", showupdate)
-
--- if this is your first time running Hydra, you're launching it more than a week later, check now
-local lastcheckedupdates = settings.get('lastcheckedupdates')
-if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
-    checkforupdates()
-end
